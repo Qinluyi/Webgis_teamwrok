8adec88209e746a1b823ac97366273f48c372731
Qinluyi <2378716408@qq.com>
2023-12-29 16:52:51 +0800

    1.0

---
+++ app.js
@@ -0,0 +1,277 @@
+const express = require('express');
+const { Client } = require('pg');
+const connectionString = 'postgres://postgres:123456@localhost:5432/MiaoXiu';
+
+const client = new Client({
+    connectionString: connectionString
+});
+
+client.connect();
+
+var app = express();
+
+app.set('port', process.env.PORT || 5500);
+
+//查询语句
+// app.get('/list', function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin','*');
+//     var name1=req.query.name1;
+//     client.query('SELECT * FROM edu_school_edu_school where localtion = $1', [name1], function (err, result) {      
+//         if (err) {
+//             console.log(err);
+//             return res.status(400).send(err);
+//         }
+//         // res.status(200).send(result.rows); 
+//         // 将查询结果存储在变量中
+//         const schoolsData = result.rows;
+
+//         // 以 JSON 格式返回数据给前端
+//         res.status(200).json(schoolsData);
+//     });
+// });
+
+// app.get('/list', function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin','*');
+//     var name1=req.query.name1;
+//     client.query('SELECT * FROM xiupin where 类别 = $1', [name1], function (err, result) {      
+//         if (err) {
+//             console.log(err);
+//             return res.status(400).send(err);
+//         }
+//         // res.status(200).send(result.rows); 
+//         // 将查询结果存储在变量中
+//         const schoolsData = result.rows;
+
+//         // 以 JSON 格式返回数据给前端
+//         res.status(200).json(schoolsData);
+//     });
+// });
+
+
+//查询语句  翻页查询
+app.get('/search_items', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var name=req.query.name;
+    var num=req.query.num;
+    var start=req.query.start;
+    client.query('SELECT * FROM xiupin1 where 类别  = $1 ORDER BY objectid LIMIT $2 OFFSET $3;', [name,num,start], function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const xiupinData = result.rows;
+        console.log("yes");
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(xiupinData);
+    });
+});
+
+
+//查询语句 单个商品信息查询
+app.get('/search_id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var object_id=req.query.object_id;
+    
+    client.query('SELECT * FROM xiupin1 where objectid  = $1;', [object_id],function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const item_Data = result.rows;
+        console.log("yes1");
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(item_Data);
+    });
+});
+
+//查询语句，查询任意3个同类商品
+app.get('/find_the_same', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var type=req.query.type;
+    
+    client.query('SELECT * FROM xiupin1 where 类别  = $1 ORDER BY RANDOM() LIMIT 3;', [type],function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const item_Data = result.rows;
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(item_Data);
+    });
+});
+//查询语句，查询8个课程
+app.get('/classes', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var start=req.query.start;
+    var num=req.query.num;
+    client.query('SELECT * FROM classes OFFSET $1 LIMIT $2;', [start,num],function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const item_Data = result.rows;
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(item_Data);
+        console.log("已请求");
+        
+    });
+});
+
+
+//查询语句，查询1个课程
+app.get('/get_class', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var class_id=req.query.class_id;
+    
+    client.query('SELECT * FROM classes where 课程id = $1;', [class_id],function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const item_Data = result.rows;
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(item_Data);
+    });
+});
+
+//查询语句，查询3个同类课程
+app.get('/get_same_classes', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    
+    
+    client.query('SELECT * FROM classes ORDER BY RANDOM() LIMIT 3;', function (err, result) {      
+        if (err) {
+            console.log(err);
+            return res.status(400).send(err);
+        }
+        // res.status(200).send(result.rows); 
+        // 将查询结果存储在变量中
+        const item_Data = result.rows;
+        // 以 JSON 格式返回数据给前端
+        res.status(200).json(item_Data);
+    });
+});
+
+
+
+
+
+//增加语句
+app.get('/list3', function(req, res) {
+	const id3 = req.query.id3;
+    const name3 = req.query.name3;
+    const code3 = req.query.code3;
+    const depart3 = req.query.depart3;
+    const city3 = req.query.city3;
+    const level3 = req.query.level3;
+    const type3 = req.query.type3;
+    const less3 = req.query.less3;
+    const remark  = "111";
+          const query = 'INSERT INTO edu_school_edu_school (id, school_name, school_code, admin_depart, localtion, school_level, edu_type, type, remark) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+          const values = [id3, name3, code3, depart3, city3, level3, type3, less3, remark];
+      
+          client.query(query, values, function(error, results)  {
+      
+            if (error) {
+              return console.error('Error executing query', error.stack);
+            }
+      
+            res.send(results.rows);
+            // Your callback logic here
+          });
+});
+
+
+
+
+// //删除语句
+
+// app.delete('/', function (req, res, next) {
+//     const schoolIdToDelete = 1; // 指定要删除的学校的ID
+
+//     client.query('DELETE FROM edu_school_edu_school WHERE id = $1', [schoolIdToDelete], function (err, result) {      
+//         if (err) {
+//             console.log(err);
+//             res.status(400).send(err);
+//         }
+
+//         // 检查是否成功删除了数据
+//         if (result.rowCount > 0) {
+//             // 成功删除
+//             res.status(200).send('School with ID ' + schoolIdToDelete + ' deleted successfully.');
+//         } else {
+//             // 没有找到要删除的数据
+//             res.status(404).send('School with ID ' + schoolIdToDelete + ' not found.');
+//         }
+//     });
+// });
+
+
+// app.get('/', function (req, res, next) {
+//     const newSchool = {
+//         id:2957,
+//         school_name:"野鸡大学",  // 从请求体中获取学校名称
+//         school_code: "13567",  // 从请求体中获取学校位置
+//         admin_depart: "江湖市",
+//         localtion: "江湖市",
+//         school_level: "大专",
+//         edu_type:"普通高等学校",
+//         type:"综合",
+//         remark:"不好"
+//     };
+
+//     client.query(
+//         'INSERT INTO edu_school_edu_school (id, school_name,school_code,admin_depart,localtion,school_level,edu_type,type,remark) VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+//         [newSchool.id, newSchool.school_name,newSchool.school_code,newSchool.admin_depart,newSchool.localtion,newSchool.school_level,newSchool.edu_type,newSchool.type,newSchool.remark],
+//         function (err, result) {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(400).send(err);
+//             }
+
+//             // 检查是否成功插入了数据
+//             if (result.rows.length > 0) {
+//                 // 成功插入，返回新插入的学校数据
+//                 res.status(200).send(result.rows[0]);
+//             } else {
+//                 // 插入失败
+//                 res.status(500).send('Failed to insert school.');
+//             }
+//         }
+//     );
+// });
+
+
+
+app.listen(5500, function () {
+    console.log('Server is running.. on Port 5500');
+});
+
+
+
+
+			// //单击查询按钮后，根据用户在文本框中输入的姓名查询数据库中的特定记录								
+            // $("#list").click(function(){
+            //     var name1 = $("#name1").val();
+            //     $.get(
+            //     "http://127.0.0.1:8000/list",
+            //     {
+            //         name1:name1
+            //     },
+            //     function(data){
+            //         for(var i=0;i<data.length;i++){
+            //             console.log(data[i]);
+            //             show(data[i]);
+            //         }
+            //     });
+            // });	
---
+++ js/ShowItem.js
@@ -0,0 +1,366 @@
+var my_switch = false;
+var id;
+var is_class;
+var lng;
+var lat;
+
+
+window.onload = function(){
+    id = localStorage.getItem("item_id"); //获取指定key本地存储的值
+    is_class = localStorage.getItem("item_type");
+    //console.log(id);
+    show_product(id,is_class);
+}
+
+
+function show_product(item_id,is_class){
+    id = item_id;
+    var type;
+    if(my_switch == true){
+        if(is_class ==0){ //如果不是class的数据
+            $.ajax({
+                url: 'http://localhost:5500/search_id',
+                type: 'get',
+                dataType: 'json',
+                data: {//传进去的值
+                    object_id:id
+                }, // Pass the parameter here
+                success: function (data) {//返回结果在data里 数据返回成功之后要干什么
+                    $.each(data, function (index, item) {
+                                    console.log(item.objectid)
+                                    $("#item_name").text(item.名字);
+                                    $("#DianZan").text(item.点赞数);
+                                    $("#XiaoLiang").text(item.销量);
+                                    var srcpath="../MiaoXiu_Image/"+item.图片;
+                                    $("#item_picture").attr("src", srcpath);
+                                    $("#item_picture").attr("class","col_up_img");
+                                    $("#description").text(item.描述);
+                                    
+                                    type=item.类别;
+                                    var map = new BMapGL.Map("map");                // 创建地图实例
+                                    var point = new BMapGL.Point(item.经度, item.纬度);     // 设置中心点坐标
+                                    lng = item.经度;
+                                    lat = item.纬度;
+                                    var marker = new BMapGL.Marker(point);        // 创建标注   
+                                    map.addOverlay(marker);   
+                                    map.centerAndZoom(point, 14);                      // 设置地图级别
+                                    map.enableScrollWheelZoom(); 
+                                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+                                    console.log('地图完成');
+                                    //找相同商品
+                                    Find_the_same(type);    
+                                });      
+                }
+            });
+        }else{
+            $.ajax({
+                url: 'http://localhost:5500/get_class',
+                type: 'get',
+                dataType: 'json',
+                data: {//传进去的值
+                    class_id:id
+                }, // Pass the parameter here
+                success: function (data) {//返回结果在data里 数据返回成功之后要干什么
+                    $.each(data, function (index, item) {
+                                    console.log(item.objectid)
+                                    $("#item_name").text(item.课程名);
+                                    $("#DianZan").text(item.点赞数);
+                                    $("#XiaoLiang").text(item.销量);
+                                    var srcpath="../MiaoXiu_Image/"+item.课程id+".jpg";
+                                    $("#item_picture").attr("src", srcpath);
+                                    $("#item_picture").attr("class","col_up_img");
+                                    $("#description").text(item.介绍);
+                                    $("#map_text").text('查看上课地点');
+                                    var map = new BMapGL.Map("map");                // 创建地图实例
+                                    var point = new BMapGL.Point(item.经度, item.纬度);     // 设置中心点坐标
+                                    lng = item.经度;
+                                    lat = item.纬度;
+                                    var marker = new BMapGL.Marker(point);        // 创建标注   
+                                    map.addOverlay(marker);   
+                                    map.centerAndZoom(point, 10);                      // 设置地图级别
+                                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+                                    console.log('地图完成');
+                                    //找相同商品
+                                    Find_the_same(0);    
+                                });      
+                }
+            });
+
+        }
+    }else{
+        if(is_class ==0){
+            fetch('../json/绣品.json')
+            .then(response => response.json()) // 解析JSON数据
+            .then(data => {
+            // 在data中查找id为8的记录
+                var num_item_id = parseInt(item_id);//转换id的数据类型
+                const item = data.find(item => item.OBJECTID === num_item_id);
+                console.log(item);
+                $("#item_name").text(item.名字);
+                type=item.类别;                
+                var srcpath="../MiaoXiu_Image/"+item.图片;
+                $("#item_picture").attr("src", srcpath);
+                $("#item_picture").attr("class","col_up_img");
+                $("#description").text(item.描述);
+                $("#map_text").text('查看绣品产地');
+                var map = new BMapGL.Map("map");                // 创建地图实例
+                var point = new BMapGL.Point(item.经度, item.纬度);     // 设置中心点坐标
+                lng = item.经度;
+                lat = item.纬度;
+                var marker = new BMapGL.Marker(point);        // 创建标注   
+                map.addOverlay(marker);   
+                map.centerAndZoom(point, 10);                      // 设置地图级别
+                map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+                console.log('地图完成');
+                Find_the_same(type); 
+
+            // 如果找到记录，则输出到控制台
+                
+            })
+            .catch(error => console.error('加载JSON文件时出错：', error));
+        }else{
+            // 使用fetch加载JSON文件
+            fetch('../json/课程.json')
+            .then(response => response.json()) // 解析JSON数据
+            .then(data => {
+            // 在data中查找id为8的记录
+                var num_item_id = parseInt(item_id);
+                const item = data.find(item => item.课程ID === num_item_id);
+                
+                
+                $("#item_name").text(item.课程名);
+                                    
+                                    var srcpath="../MiaoXiu_Image/"+item.课程ID+".jpg";
+                                    $("#item_picture").attr("src", srcpath);
+                                    $("#item_picture").attr("class","col_up_img");
+                                    $("#description").text(item.介绍);
+                                    $("#map_text").text('查看上课地点');
+                                    var map = new BMapGL.Map("map");                // 创建地图实例
+                                    var point = new BMapGL.Point(item.经度, item.纬度);     // 设置中心点坐标
+                                    lng = item.经度;
+                                    lat = item.纬度;
+                                    var marker = new BMapGL.Marker(point);        // 创建标注   
+                                    map.addOverlay(marker);   
+                                    map.centerAndZoom(point, 10);                      // 设置地图级别
+                                    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+                                    console.log('地图完成');
+                                    Find_the_same(0); 
+
+            // 如果找到记录，则输出到控制台
+                
+            })
+            .catch(error => console.error('加载JSON文件时出错：', error));
+        }
+    }
+}
+function Find_the_same(type){
+    tbodydata='';
+    if(my_switch==true){
+        if(type !=0){
+            console.log('找相同');
+            
+            $.ajax({
+                url: 'http://localhost:5500/find_the_same',
+                type: 'get',
+                dataType: 'json',
+                data: {//传进去的值
+                    type: type,
+                }, // Pass the parameter here
+                success: function (data) {//返回结果在data里 数据返回成功之后要干什么
+                    console.log('找到相同');
+                    tbodydata=DivData(data,type);//给tbodydata赋值
+                    $("#same_items").html(tbodydata);  //给相同的商品赋值
+                    // tbodydata = "<div class='box_contain'>";
+                    // $.each(data, function (index, item) {
+                    //     if(type==1){
+                    //                 tbodydata +="<div class='pic_box'>"+"<img src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                    //                 "<div class = 'name_img'><p>"+item.非遗种+"</p>"+
+                    //                 "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                    //                 "<hr/><p>"+item.名字+"</p>"+"</div>";
+                    //                 if(index == 2){
+                    //                     tbodydata+="</div>";
+                    //                 }
+                    //     }
+                    // if(type == 2){
+                        
+                    //                 tbodydata +="<div class='pic_box'>"+"<img src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                    //                 "<div class = 'name_img'><p>"+item.品牌+"</p>"+
+                    //                 "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                    //                 "<hr/><p>"+item.名字+"</p>"+"</div>";
+                    //                 if(index == 2){
+                    //                     tbodydata+="</div>";
+                    //                 }      
+                    // }
+                    // if(type == 3){
+                        
+                    //                 tbodydata +="<div class='pic_box'>"+"<img src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                    //                 "<div class = 'name_img'><p>"+item.寓意+"</p>"+
+                    //                 "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                    //                 "<hr/><p>"+item.名字+"</p>"+"</div>";
+                    //                 if(index == 2){
+                    //                     tbodydata+="</div>";
+                    //                 }
+                    // }        
+                    // });  
+                    // $("#same_items").html(tbodydata);    
+                }
+            });
+        }else{
+            console.log('找课程相同');
+            $.ajax({
+                url: 'http://localhost:5500/get_same_classes',
+                type: 'get',
+                dataType: 'json',
+                data: {//传进去的值
+                    
+                }, // Pass the parameter here
+                success: function (data) {//返回结果在data里 数据返回成功之后要干什么
+                    tbodydata = "<div class='box_contain'>";
+                    $.each(data, function (index, item) {
+                        tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.课程id+".jpg' alt='"+item.课程id+".jpg' onclick='show_product("+item.课程id+",1)'>"+
+                        "<div class = 'name_img'><p>"+item.课程名+"</p>"+
+                        "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.课程id+"' onclick='add_product("+item.课程id+")'></div></div>";
+                        if(index == 2){
+                            tbodydata+="</div>";
+                        }           
+                    });  
+                    $("#same_items").html(tbodydata);     
+                }
+            });
+        } 
+    }else{
+        if(type !=0){
+            fetch('../json/绣品.json')
+            .then(response => response.json())
+            .then(jsonData => {
+                // 筛选出 type 为 1 的记录
+                const type1Records = jsonData.filter(item => item.类别 === type);
+                // 随机选取 3 条记录
+                const randomType1Records = getRandomRecords(type1Records, 3);
+
+                $.each(randomType1Records, function (index, item) {
+                    console.log(item.价格);
+                });
+
+                tbodydata=DivData(randomType1Records,type);//给tbodydata赋值
+                $("#same_items").html(tbodydata);  //给相同的商品赋值
+                //console.log(randomType1Records);
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        }
+        if(type==0){//如果是课程
+            fetch('../json/课程.json')
+            .then(response => response.json()) // 2. 解析 JSON 数据
+            .then(data => {
+                // 3. 随机选择三条记录
+                const items = getRandomRecords(data, 3);
+                tbodydata = "<div class='box_contain'>";
+                    for(var i=0;i<3;i++){
+                        tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+items[i]["课程ID"]+".jpg' alt='"+items[i]["课程ID"]+".jpg' onclick='show_product("+items[i]["课程ID"]+",1)'>"+
+                        "<div class = 'name_img'><p>"+items[i]["课程名"]+"</p>"+
+                        "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+items[i]["课程ID"]+"' onclick='add_product("+items[i]["课程ID"]+")'></div></div>";
+                        if(i == 2){
+                            tbodydata+="</div>";
+                        }           
+                    }  
+                    $("#same_items").html(tbodydata);  
+            })
+            .catch(error => console.error('Error fetching JSON:', error));
+        }
+    }
+}
+
+
+function DivData(data,type){
+
+    var tbodydata = "<div class='box_contain'>";
+    if(my_switch==true){
+                    $.each(data, function (index, item) {
+                        if(type==1){
+                                    tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                                    "<div class = 'name_img'><p>"+item.非遗种+"</p>"+
+                                    "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                                    "<hr/><p>"+item.名字+"</p>"+"</div>";
+                                    if(index == 2){
+                                        tbodydata+="</div>";
+                                    }
+                        }
+                    if(type == 2){
+                        
+                                    tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                                    "<div class = 'name_img'><p>"+item.品牌+"</p>"+
+                                    "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                                    "<hr/><p>"+item.名字+"</p>"+"</div>";
+                                    if(index == 2){
+                                        tbodydata+="</div>";
+                                    }      
+                    }
+                    if(type == 3){
+                        
+                                    tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.objectid+",0)'>"+
+                                    "<div class = 'name_img'><p>"+item.寓意+"</p>"+
+                                    "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.objectid+"' onclick='add_product("+item.objectid+")'></div>"+
+                                    "<hr/><p>"+item.名字+"</p>"+"</div>";
+                                    if(index == 2){
+                                        tbodydata+="</div>";
+                                    }
+                    }        
+                    });  
+           
+    }else{
+        $.each(data, function (index, item) {
+            if(type==1){
+                            tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.OBJECTID+",0)'>"+
+                            "<div class = 'name_img'><p>"+item.非遗种+"</p>"+
+                            "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.OBJECTID+"' onclick='add_product("+item.OBJECTID+")'></div>"+
+                            "<hr/><p>"+item.名字+"</p>"+"</div>";
+                            if(index == 2){
+                                tbodydata+="</div>";
+                            }
+                }
+            if(type == 2){
+                
+                            tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.OBJECTID+",0)'>"+
+                            "<div class = 'name_img'><p>"+item.品牌+"</p>"+
+                            "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.OBJECTID+"' onclick='add_product("+item.OBJECTID+")'></div>"+
+                            "<hr/><p>"+item.名字+"</p>"+"</div>";
+                            if(index == 2){
+                                tbodydata+="</div>";
+                            }      
+            }
+            if(type == 3){
+                
+                            tbodydata +="<div class='pic_box'>"+"<img class='box_img' src='../MiaoXiu_Image/"+item.图片+"' alt='"+item.图片+"' onclick='show_product("+item.OBJECTID+",0)'>"+
+                            "<div class = 'name_img'><p>"+item.寓意+"</p>"+
+                            "<img src='../imgs/购物车.png' class='add-to-cart' alt='Image' item_id='"+item.OBJECTID+"' onclick='add_product("+item.OBJECTID+")'></div>"+
+                            "<hr/><p>"+item.名字+"</p>"+"</div>";
+                            if(index == 2){
+                                tbodydata+="</div>";
+                            }
+            }        
+        });  
+    } 
+    return tbodydata;  
+}
+
+
+function getRandomRecords(data, count) {
+    const shuffled = data.sort(() => 0.5 - Math.random()); // 随机排序
+    return shuffled.slice(0, count); // 截取前 count 条记录
+}
+
+//和购物车有关功能
+var cartItems = [];
+var cartCount = 0;
+var is_add = false;
+
+function add_product(id){
+    console.log(id);  
+}
+
+
+function To_Map(){
+    localStorage.setItem('lng', lng);
+    localStorage.setItem('lat', lat);
+    window.location.href = 'fangting.html';//芳婷的网页
+}
