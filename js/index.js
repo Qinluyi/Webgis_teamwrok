98fbd1aa082a21b492f33d4ff81750676583f889
zimohe <1907774602@qq.com>
2024-01-02 18:05:16 +0800

    产业概览
    终版

---
+++ js/index.js
@@ -0,0 +1,306 @@
+/**
+ * Created by 30947 on 2018/7/18.
+ */
+$(function(){
+
+    char1();
+    char2();
+    char3();
+    char4();
+
+})
+
+//统计分析图
+function char1() {
+
+    var myChart = echarts.init($("#char1")[0]);
+
+    option = {
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient : 'vertical',
+            x : 'right',
+            textStyle : {
+                color : '#ffffff',
+
+            },
+            data:['雷山苗绣','松桃苗绣','湘西苗绣','剑河苗绣']
+        },
+
+        calculable : false,
+        series : [
+            {
+                name:'绣品类型',
+                type:'pie',
+                radius : ['40%', '70%'],
+                itemStyle : {
+                    normal : {
+                        label : {
+                            show : false
+                        },
+                        labelLine : {
+                            show : false
+                        }
+                    },
+                    emphasis : {
+                        label : {
+                            show : true,
+                            position : 'center',
+                            textStyle : {
+                                fontSize : '20',
+                                fontWeight : 'bold'
+                            }
+                        }
+                    }
+                },
+                data:[
+                    {value:335, name:'雷山苗绣'},
+                    {value:310, name:'松桃苗绣'},
+                    {value:234, name:'湘西苗绣'},
+                    {value:135, name:'剑河苗绣'}
+
+                ]
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+    window.addEventListener('resize', function () {myChart.resize();})
+
+}
+function char2() {
+
+    var myChart = echarts.init($("#char2")[0]);
+
+    option = {
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {show:'true',borderWidth:'0'},
+        legend: {
+            data:['单笔', '批发','固定合作','新增合作'],
+            textStyle : {
+                color : '#ffffff',
+
+            }
+        },
+
+        calculable :false,
+        xAxis : [
+            {
+                type : 'value',
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                splitLine:{
+                    lineStyle:{
+                        color:['#f2f2f2'],
+                        width:0,
+                        type:'solid'
+                    }
+                }
+
+            }
+        ],
+        yAxis : [
+            {
+                type : 'category',
+                data : ['水云绣品','舞绣','月湘绣','邻家绣品'],
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                splitLine:{
+                    lineStyle:{
+                        width:0,
+                        type:'solid'
+                    }
+                }
+            }
+        ],
+        series : [
+            {
+                name:'单笔',
+                type:'bar',
+                stack: '总量',
+                itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+                data:[320, 302, 301, 334]
+            },
+            {
+                name:'批发',
+                type:'bar',
+                stack: '总量',
+                itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+                data:[120, 132, 101, 134]
+            },
+            {
+                name:'固定合作',
+                type:'bar',
+                stack: '总量',
+                itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+                data:[220, 182, 191, 234]
+            },
+            {
+                name:'新增合作',
+                type:'bar',
+                stack: '总量',
+                itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+                data:[150, 212, 201, 154]
+            }
+
+        ]
+    };
+
+    myChart.setOption(option);
+    window.addEventListener('resize', function () {myChart.resize();})
+
+}
+function char3() {
+
+    var myChart = echarts.init($("#char3")[0]);
+
+    option = {
+        legend: {
+            data:['单日成交量'],
+            textStyle : {
+                color : '#ffffff',
+
+            }
+        },
+        grid: {show:'true',borderWidth:'0'},
+
+        calculable : false,
+        tooltip : {
+            trigger: 'axis',
+            formatter: "成交额 : <br/>{b} : {c}万元"
+        },
+        xAxis : [
+            {
+                type : 'value',
+                axisLabel : {
+                    formatter: '{value}',
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+
+                splitLine:{
+                    lineStyle:{
+                        width:0,
+                        type:'solid'
+                    }
+                }
+            }
+        ],
+        yAxis : [
+            {
+                type : 'category',
+                axisLine : {onZero: false},
+                axisLabel : {
+                    formatter: '{value} ',
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                splitLine:{
+                    lineStyle:{
+                        width:0,
+                        type:'solid'
+                    }
+                },
+                boundaryGap : false,
+                data : ['12.20', '12.21', '12.22', '12.23', '12.24', '12.25', '12.26', '12.27', '12.28']
+            }
+        ],
+        series : [
+            {
+                name:'单日成交量',
+                type:'line',
+                smooth:true,
+                itemStyle: {
+                    normal: {
+                        lineStyle: {
+                            shadowColor : 'rgba(0,0,0,0.4)'
+                        }
+                    }
+                },
+                data:[10, 5, 25, 17, 33, 20, 40, 28, 23]
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+    window.addEventListener('resize', function () {myChart.resize();})
+
+}
+function char4() {
+
+    var myChart = echarts.init($("#char4")[0]);
+
+    option = {
+        grid: {show:'true',borderWidth:'0'},
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            },
+
+            formatter: function (params) {
+                var tar = params[0];
+                return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+            }
+        },
+
+        xAxis : [
+            {
+                type : 'category',
+                splitLine: {show:false},
+                data : ['遵义','贵阳','毕节','铜仁'],
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                }
+
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                splitLine: {show:false},
+                axisLabel: {
+                    show: true,
+                    textStyle: {
+                        color: '#fff'
+                    }
+                }
+            }
+        ],
+        series : [
+
+            {
+                name:'成交额',
+                type:'bar',
+                stack: '总量',
+                itemStyle : { normal: {label : {show: true, position: 'inside'}}},
+                data:[2900, 1200, 2000, 1500, 900, 300]
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+    window.addEventListener('resize', function () {myChart.resize();})
+
+}
